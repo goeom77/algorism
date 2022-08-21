b0f105d650c2da920c12a75e8025a662b49687aa
goeom77 <7eom14@gmail.com>
2022-08-21 17:51:47 +0900

    0821r

--- 1517.py
+++ 1517.py
@@ -5,9 +5,13 @@ num = int(input())
 arr = sys.stdin.readline().rstrip().split()
 n = len(arr)
 search = 0
+stack = 0
 for i in range(n):
-    search += (int(arr[i])-i-1)
-    print(search)
+    plus = arr[i] -i -1
+    if plus < 0 :
+        plus = 0
+    if stack != 0:
+        search += plus
 print(search)
 
 
